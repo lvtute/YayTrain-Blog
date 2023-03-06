7a351c100cb38267f8d84c1ec566c88e9ef23a68
Shelby <lvtute@gmail.com>
2023-03-06 11:52:42 +0700

    done saving post views count

--- includes/frontend/post.php
+++ includes/frontend/post.php
@@ -4,22 +4,28 @@ add_filter('the_content', 'update_post_views');
 
 function update_post_views($content)
 {
-    if (is_single()) {
-        $post_id = get_the_ID();
-
-        $count_key = 'post_views_count';
-        $count = get_post_meta($post_id, $count_key, true);
-
-        if (!$count) {
-            delete_post_meta($post_id, $count_key);
-            add_post_meta($post_id, $count_key, 1);
-            return;
-        }
-
-        $count++;
-
-        update_post_meta($post_id, $count_key, $count);
+    if (is_user_logged_in()) {
+        return;
     }
+
+    if (!is_single()) {
+        return;
+    }
+
+    $post_id = get_the_ID();
+
+    $count_key = 'post_views_count';
+    $count = get_post_meta($post_id, $count_key, true);
+
+    if (!$count) {
+        delete_post_meta($post_id, $count_key);
+        add_post_meta($post_id, $count_key, 1);
+        return;
+    }
+
+    $count++;
+
+    update_post_meta($post_id, $count_key, $count);
 
     return $content;
 }
